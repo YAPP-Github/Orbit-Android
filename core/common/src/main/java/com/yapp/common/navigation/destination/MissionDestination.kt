ad6f58fcd69485cea895f05be6edd4abe3b5fe82
MoonsuKang <rkdms4971@naver.com>
2025-04-23 20:15:41 +0900

    [DEL/#215] 미션 페이지 축소로 인한 목적지 수정

--- core/common/src/main/java/com/yapp/common/navigation/destination/MissionDestination.kt
+++ core/common/src/main/java/com/yapp/common/navigation/destination/MissionDestination.kt
@@ -5,9 +5,4 @@ import com.yapp.common.navigation.Routes
 sealed class MissionDestination(val route: String) {
     data object Route : MissionDestination(Routes.Mission.ROUTE)
     data object Mission : MissionDestination(Routes.Mission.MISSION)
-    data object Progress : MissionDestination(Routes.Mission.PROGRESS)
-
-    companion object {
-        val routes = listOf(Mission, Progress)
-    }
 }
