361518ebf0e6e9d6fb5ccdb36a25fdfeac3c35da
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-28 11:04:28 +0900

    [RELEASE] v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
     namespace = "com.yapp.orbit"
 
     defaultConfig {
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
         targetSdk = 34
     }
 
