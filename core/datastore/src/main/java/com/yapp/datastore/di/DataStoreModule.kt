3adec81b798844a6c895278e25daa2f5c3f1d173
dongchyeon <kimdonghyun90766@gmail.com>
2025-06-10 20:15:28 +0900

    [REMOVE/#222] 사용하지 않는 토큰 관련 코드 제거

--- core/datastore/src/main/java/com/yapp/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/com/yapp/datastore/di/DataStoreModule.kt
@@ -2,12 +2,9 @@ package com.yapp.datastore.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import com.yapp.datastore.token.AuthToken
-import com.yapp.datastore.token.TokenDataSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,18 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
-    @Provides
-    @Singleton
-    fun providesTokenDataStore(
-        @ApplicationContext context: Context,
-        tokenDataSerializer: TokenDataSerializer,
-    ): DataStore<AuthToken> =
-        DataStoreFactory.create(
-            serializer = tokenDataSerializer,
-        ) {
-            context.dataStoreFile("token.json")
-        }
-
     @Provides
     @Singleton
     fun providesPreferencesDataStore(
