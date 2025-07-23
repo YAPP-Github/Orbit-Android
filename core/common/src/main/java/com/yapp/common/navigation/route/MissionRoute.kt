e424de0fefdca89a0d1dc28f25a244a0904ada51
dongchyeon <kimdonghyun90766@gmail.com>
2025-07-23 21:31:55 +0900

    [FEAT/#235] 미션 미리보기 기능 추가

--- core/common/src/main/java/com/yapp/common/navigation/route/MissionRoute.kt
+++ core/common/src/main/java/com/yapp/common/navigation/route/MissionRoute.kt
@@ -3,4 +3,12 @@ package com.yapp.common.navigation.route
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object MissionRoute
+data class MissionRoute(
+    val missionType: String,
+    val missionCount: String,
+    val missionMode: String = "REAL", // PREVIEW 지원
+) {
+    companion object {
+        const val route = "mission"
+    }
+}
